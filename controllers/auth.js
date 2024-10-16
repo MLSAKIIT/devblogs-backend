3a1c31077bed0b8e7c04a567e748599fe9d3be9d
geekyharsh05 <hp424805@gmail.com>
2024-10-16 18:27:12 +0530

    feat: add ratelimiting and request logger.

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const loginHandler = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = User.findOne({ email });
+    const user = await User.findOne({ email });
     // TODO : verify user
     if (!user) {
       return res.status(400).json({ message: "Invalid email or password" });
@@ -18,38 +18,41 @@ const loginHandler = async (req, res) => {
       return res.status(400).json({ message: "Invalid email or password" });
     }
 
-
     const token = jwtHelper.createToken({ email: user.email });
-    res.status(200).json({ token });
+    return res
+      .status(200)
+      .json({ message: "User Logged In Successfully", token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error" });
+    return res.status(500).json({ message: "Server error" });
   }
 };
 
 const registerHandler = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json({ message: "Email and password are required" });
-    }
-    const findUser=await User.findOne({email});
-    if(findUser){
-      return res.status(403).json({message:"User Already exists"})
-    }
-    let hashedPassword;
-    try {
-      hashedPassword = await bcrypt.hash(password, 12);
-    } catch (error) {
-      console.error("Error hashing password:", error);
-      return res.status(500).json({ message: "Error hashing password" });
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Some fields are missing" });
     }
 
-    const newUser=new User({
+    const findUser = await User.findOne({ email });
+    if (findUser) {
+      return res.status(403).json({ message: "User Already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    if (!hashedPassword) {
+      return res.status(500).json({ message: "Error in hashing password" });
+    }
+
+    const newUser = new User({
+      username,
       email,
-      password:hashedPassword
-    })
+      password: hashedPassword,
+    });
+
     await newUser.save();
+
     return res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     console.error(error);
@@ -59,7 +62,7 @@ const registerHandler = async (req, res) => {
 
 const verifyTokenHandler = (req, res) => {
   const token = req.body.token || req.headers["authorization"];
-  
+
   if (!token) {
     return res.status(400).json({ message: "Token is required" });
   }
