95cba5c0f236458837213201b22b756140e116ad
solo8116 <gr3y.ma7t3r@gmail.com>
2024-10-22 15:07:36 +0530

    fix: req.userId undefined in verifyTokenMiddleware

--- middlewares/verifyTokenMiddleware.js
+++ middlewares/verifyTokenMiddleware.js
@@ -5,7 +5,7 @@ const token = req.header('Authorization');
 if (!token) return res.status(401).json({ error: 'Access denied' });
 try {
  const decoded = jwt.verify(token, process.env.JWT_SECRET);
- req.userId = decoded.userId;
+ req.userId = decoded.payload;
  next();
 } catch (error) {
  res.status(401).json({ error: 'Invalid token' });
