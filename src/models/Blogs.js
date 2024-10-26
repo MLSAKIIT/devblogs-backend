0cbde036f609a0c7c7beda720d4de6e3bd99b4e4
solo8116 <gr3y.ma7t3r@gmail.com>
2024-10-26 13:23:12 +0530

    add timestamps to blog and user model

--- src/models/Blogs.js
+++ src/models/Blogs.js
@@ -4,6 +4,8 @@ const blogsSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+},{
+  timestamps: true
 });
 
 export default mongoose.model("Blog", blogsSchema);
--- src/models/User.js
+++ src/models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+},{
+  timestamps: true
 });
 
 export default mongoose.model("User", userSchema);
